ccf644fd71c0cc1aea66bde496a7aea82730558e
Ben Lachman <ben@nicemohawk.com>
2015-07-02 13:27:29 -0400

    Stubbing out AboutViewController + live app title
    Closes #18

---
+++ Brew Week/Brew Week/AboutViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AboutViewController.swift
+//  Brew Week
+//
+//  Created by Ben Lachman on 7/2/15.
+//  Copyright (c) 2015 Ohio Brew Week. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+	@IBOutlet weak var appNameAndVersionLabel: UILabel!
+	@IBOutlet weak var athensworksButton: UIButton!
+
+	override func viewDidLoad() {
+		let appInfoDictionary = NSBundle.mainBundle().infoDictionary
+		let bundleVersion = appInfoDictionary![kCFBundleVersionKey] as! NSString
+		let bundleShortVersion = appInfoDictionary!["CFBundleShortVersionString"] as! NSString
+
+		appNameAndVersionLabel.text = "Ohio Brew Week \(bundleShortVersion) (v\(bundleVersion))"
+	}
+
+	@IBAction func openAthensworksAction(sender: UIButton) {
+
+	}
+}
