86117c1375fac09fe4112295eeba52aea756e9a4
Ben Lachman <ben@nicemohawk.com>
2015-07-03 13:57:13 -0400

    Cleaning up a bunch of project structure stuff.

--- Brew Week/Brew Week/Pods/Target Support Files/Pods-Brew Week/Pods-Brew Week-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Brew_Week : NSObject
-@end
-@implementation PodsDummy_Pods_Brew_Week
-@end
--- Brew Week/Brew Week/Pods/Target Support Files/Pods-Brew Week/Pods-Brew Week-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Brew_WeekVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Brew_WeekVersionString[];
-
